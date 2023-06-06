5f66ac571f0631219511b03402d3e4cfdf659a6b
Johan Meza <mezajohan0604@gmail.com>
2023-06-05 21:34:54 -0400

    initial

--- config/db.js
+++ config/db.js
@@ -1,21 +1,25 @@
-import Sequelize from 'sequelize';
-import dotenv from "dotenv/config";
-
-
-const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+const db = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
     host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
-    dialect: 'mysql',
+    dialect: "mysql",
     define: {
-        timestamps: false
+      timestamps: false,
     },
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000, // tiempo de espera maximo para conectarse a la base de datos
-        idle: 10000 // tiempo maximo de inactividad de la conexion
+      max: 5,
+      min: 0,
+      acquire: 30000, // tiempo de espera maximo para conectarse a la base de datos
+      idle: 10000, // tiempo maximo de inactividad de la conexion
     },
-    operatorAliases: false
-})
+    operatorAliases: false,
+  }
+);
 
 export default db;
