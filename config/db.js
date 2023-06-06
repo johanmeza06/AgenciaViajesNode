4b617498e8e0f75762208d8736d69191b192b76c
Johan Meza <mezajohan0604@gmail.com>
2023-06-05 21:30:16 -0400

    "deployment"

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import Sequelize from 'sequelize';
 import dotenv from "dotenv/config";
 
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: '3306',
+const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     define: {
         timestamps: false
